24ce65d3e599f848f404107d4073424806cb44eb
Dhifaf Athiyah Zhabiyan <54610871+dhifafaz@users.noreply.github.com>
2023-06-02 23:56:10 +0700

    fixing the api response by adding metadata

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,29 +1,38 @@
 import UserModel from "../models/UserModel.js";
 
 export const createUser = async (req, res) => {
-	const { firstName, lastName, birthday, location } = req.body;
-	// const data = req.body;
+	const data = req.body;
 
 	const user = new UserModel({
-		// firstName: data.firstName || "",
-		// lastName: data.lastName || "",
-		// birthday: data.birthday || "",
-		// location: data.location || "",
-		firstName: firstName || "",
-		lastName: lastName || "",
-		birthday: birthday || "",
-		location: location || "",
+		firstName: data.firstName || "",
+		lastName: data.lastName || "",
+		birthday: data.birthday || "",
+		location: data.location || "",
 	});
 
 	try {
 		await user.save().then((response) => {
 			res.status(201).json({
-				message: "User Created",
-				id: response.id,
+				meta: {
+					code: 201,
+					success: true,
+					message: "User Created",
+				},
+				data: {
+					id: response._id,
+					firstName: response.firstName,
+					lastName: response.lastName,
+					birthday: response.birthday,
+					location: response.location,
+				},
 			});
 		});
 	} catch (error) {
 		console.log(error.message);
-		res.status(404).json({ message: error.message });
+		res.status(400).json({
+			code: 400,
+			success: false,
+			message: error.message,
+		});
 	}
 };
